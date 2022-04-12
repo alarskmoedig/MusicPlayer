b74671fc9f6e0b88998e8f06724144150ea2205d
ALARS K MOEDIG <89094414+alarskmoedig@users.noreply.github.com>
2022-04-12 17:47:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ from core import (
 
 REPO = """
 🤖 **Music Player**
-- Repo: [GitHub](https://github.com/AsmSafone/MusicPlayer)
+- Repo: [GitHub](https://github.com/alarskmoedig/MusicPlayer)
 - License: AGPL-3.0-or-later
 """
 
